d14e61f84741ad1dab69fb80ca312cb29ac15e53
Jessicavandermarel <vandermareljessica@gmail.com>
2017-12-11 13:54:30 -0500

    added action to form

--- js/contact-form.js
+++ js/contact-form.js
@@ -5,13 +5,14 @@ function submitForm(){
 	var formdata = new FormData();
 	formdata.append( "n", _("n").value );
 	formdata.append( "e", _("e").value );
-	formdata.append( "m", _("m").value );
+	formdata.append( "p", _("m").value );
+  formdata.append( "m", _("m").value );
 	var ajax = new XMLHttpRequest();
 	ajax.open( "POST", "example_parser.php" );
 	ajax.onreadystatechange = function() {
 		if(ajax.readyState == 4 && ajax.status == 200) {
 			if(ajax.responseText == "success"){
-				_("my_form").innerHTML = '<h2>Thanks '+_("n").value+', your message has been sent.</h2>';
+				_("my_form").innerHTML = '<h2>Bedankt '+_("n").value+', Je bericht is verzonden.</h2>';
 			} else {
 				_("status").innerHTML = ajax.responseText;
 				_("mybtn").disabled = false;
