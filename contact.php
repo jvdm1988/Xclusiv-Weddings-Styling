699e8205273c5e19964c6d5ea12a4ff86bbe4366
Jessicavandermarel <vandermareljessica@gmail.com>
2017-12-12 16:08:02 -0500

    changed php extension

--- contact.php
+++ contact.php
@@ -1,3 +1,58 @@
+<?php
+	// Message Vars
+	$msg = '';
+	$msgClass = '';
+
+	// Check For Submit
+	if(filter_has_var(INPUT_POST, 'submit')){
+		// Get Form Data
+		$name = htmlspecialchars($_POST['name']);
+		$email = htmlspecialchars($_POST['email']);
+		$message = htmlspecialchars($_POST['message']);
+
+		// Check Required Fields
+		if(!empty($email) && !empty($name) && !empty($message)){
+			// Passed
+			// Check Email
+			if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+				// Failed
+				$msg = 'Please use a valid email';
+				$msgClass = 'alert-danger';
+			} else {
+				// Passed
+				$toEmail = 'support@traversymedia.com';
+				$subject = 'Contact Request From '.$name;
+				$body = '<h2>Contact Request</h2>
+					<h4>Name</h4><p>'.$name.'</p>
+					<h4>Email</h4><p>'.$email.'</p>
+					<h4>Message</h4><p>'.$message.'</p>
+				';
+
+				// Email Headers
+				$headers = "MIME-Version: 1.0" ."\r\n";
+				$headers .="Content-Type:text/html;charset=UTF-8" . "\r\n";
+
+				// Additional Headers
+				$headers .= "From: " .$name. "<".$email.">". "\r\n";
+
+				if(mail($toEmail, $subject, $body, $headers)){
+					// Email Sent
+					$msg = 'Your email has been sent';
+					$msgClass = 'alert-success';
+				} else {
+					// Failed
+					$msg = 'Your email was not sent';
+					$msgClass = 'alert-danger';
+				}
+			}
+		} else {
+			// Failed
+			$msg = 'Please fill in all fields';
+			$msgClass = 'alert-danger';
+		}
+	}
+?>
+
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
@@ -10,7 +65,8 @@
 <meta charset="utf-8">
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Xclusiv | Styling</title>
+<title>Xclusiv | Contact</title>
+<link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css">
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/flexslider.css">
 <link rel="stylesheet" href="css/main.css">
@@ -74,13 +130,25 @@
     <div class="row">
       <div class="col-sm-12 col-md-10 col-md-offset-1 work-detail-margin detail-image">
         <div class="container">
-          <form id="my_form" action="mail.php" method="POST" onsubmit="submitForm(); return false;">
-            <p><input id="n" placeholder="Naam" required></p>
-            <p><input id="e" placeholder="Email" type="email" required></p>
-            <p><input id="p" placeholder="Telefoon" type="text" required></p>
-            <textarea id="m" placeholder="Bericht" rows="10" required></textarea>
-            <p><input id="mybtn" type="submit" value="Verzend"> <span id="status"></span></p>
-          </form>
+					<?php if($msg != ''): ?>
+		    		<div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
+		    	<?php endif; ?>
+					<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+			      <div class="form-group">
+				      <label>Name</label>
+				      <input type="text" name="name" class="form-control" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
+			      </div>
+			      <div class="form-group">
+			      	<label>Email</label>
+			      	<input type="text" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
+			      </div>
+			      <div class="form-group">
+			      	<label>Message</label>
+			      	<textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
+			      </div>
+			      <br>
+			      <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+		      </form>
         </div>
       </div>
   </div>
